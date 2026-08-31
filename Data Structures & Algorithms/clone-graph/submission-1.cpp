f122c621caead88e473226fe2869d0bb205e32ee
Kush Gupta <84303806+Guptak12@users.noreply.github.com>
2026-08-31 13:32:47 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    Node* dfs(Node* node,unordered_map<Node*,Node*> &vis){
+        if(vis.count(node)) return vis[node];
+        Node* next_res = new Node(node->val);
+        vis[node]=next_res;
+        for(auto n:node->neighbors){     
+            next_res->neighbors.push_back(dfs(n,vis));
+        }
+        return next_res;
+    }
+
+    Node* cloneGraph(Node* node) {
+        if(node==NULL) return NULL;
+        unordered_map<Node*,Node*> vis;
+        return dfs(node,vis);
+    }
+};
